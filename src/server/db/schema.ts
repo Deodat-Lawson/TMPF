6045117d268166ad2205a37ad78bc54b4ca0d2f2
timot <timothylinziqi@gmail.com>
2025-01-13 16:44:37 -0500

    added users and tasks database

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -22,14 +22,42 @@ export const users = createTable(
   "users",
   {
     id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    userName: varchar("name", { length: 256 }).notNull(),
-    userId: varchar("userId", { length: 256 }).notNull(),
+    userName: varchar("username", { length: 256 }).notNull(),
+    userId: varchar("user_authentication_Id", { length: 256 }).notNull(),
 
-    createdAt: timestamp("created_at", { withTimezone: true }).default(sql`CURRENT_TIMESTAMP`).notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(() => new Date()),
-
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
   },
   (users) => ({
-    uniqueUserId: index(users.userId).unique(),
+    nameIndex: index("userId_idx").on(users.userId),
   })
+);
+
+export const tasks = createTable("tasks", {
+  id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+
+  // Foreign key referencing persons.id
+  userId: integer("user_id").notNull().references(users.id),
+
+  name: varchar("name", { length: 256 }).notNull(),
+  timeSpent: integer("time_spent").notNull(),
+  timeRemaining: integer("time_remaining").notNull(),
+  mode: varchar("mode", { length: 256 }).notNull(),
+  category: varchar("category", { length: 256 }).notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+    () => new Date(),
+  ),
+},
+  (task) => ({
+    personIdIndex: index("task_person_id_idx").on(task.userId),
+    categoryIndex: index("task_category_idx").on(task.category),
+  })
+
 );
