6b8aae60f7f99173b85ba77735a472503ccec85b
timot <timothylinziqi@gmail.com>
2025-01-17 16:31:45 -0500

    Updated Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,11 @@ import {type Metadata} from "next";
 import Link from "next/link";
 import {Navbar} from "~/app/_components/Navbar";
 import {ClerkProvider} from "@clerk/nextjs";
+const LOGO_URL = "/logo2.png";
 
 export const metadata: Metadata = {
   title: "NORA AI",
-  icons: [{rel: "icon", url: "/favicon.ico"}],
+  icons: [{rel: "icon", url: LOGO_URL}],
 };
 
 
